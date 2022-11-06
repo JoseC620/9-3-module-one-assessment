f3548052ee287cf7e7d68a57ef751e2115764a06
JoseC620 <Jcepeda6201@gmail.com>
2022-11-06 09:51:33 -0500

    fourth test done'

--- index.js
+++ index.js
@@ -96,7 +96,16 @@ function getAverageIMDBRating(movies) {
  *  countByRating(movies);
  *  //> { G: 3, PG: 7 }
  */
-function countByRating() {}
+function countByRating(movies) {
+  let obj = {}
+  for (let i = 0; i < movies.length; i++){
+    if(!obj[movies[i].rated]){
+      obj[movies[i].rated] = 1
+    } else 
+    obj[movies[i].rated] += 1
+  }
+  return obj
+}
 
 /**
  * findById()
